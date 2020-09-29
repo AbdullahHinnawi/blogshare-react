75fc8a2f77cf99a780c334c6a9e0a86c16dac1bb
AbdullahHinnawi <hinnawiabdullah@gmail.com>
2020-09-29 16:56:41 +0300

    form validation added for register and login components

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -1,28 +1,101 @@
-import React from "react";
+import React, {useState} from 'react';
 import styled from "styled-components";
+import axios from 'axios';
 
 
 const Login = () =>{
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const usernameError = () => {
+    return username === '';
+  };
+  const passwordError = () => {
+    return password === '';
+  };
+
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+    if(usernameError() || passwordError()){
+      setError(true);
+      setSuccess(false);
+    }else {
+      // Password confirmation is only on the client side.
+      // There is no need to send the password confirmation field with the user object to the server.
+      const loginRequest = {
+        username: username,
+        password: password,
+
+      };
+
+      setUsername('');
+      setPassword('');
+
+      console.log("NEW loginRequest : ", loginRequest);
+      axios.post("", loginRequest).then(res => {
+        console.log("#### loginRequest response ", res);
+        setError(false);
+        setSuccess(true);
+        setSubmitted(false);
+
+      }).catch(err => {
+        console.log("Error: ", err);
+      });
+    }
+
+  };
 
     return(
 
         <Styles>
           <div style={{marginTop:'50px'}} className="login">
             <h2 className="custom-form">Log In</h2>
-            <form className="custom-form">
+            <form className="custom-form" onSubmit={onSubmit}>
 
               <div className="form-group">
                 <label htmlFor="username">Username</label>
                 <input type="text"
                        className="form-control" id="username" name="username"
-                       placeholder="Username"/>
+                       placeholder="Username"
+                       value={username}
+                       onChange={handleUsernameChange}
+                />
+                <span className="errorMessage">
+                  {submitted && usernameError() ? 'Username is required' : ''}
+
+                </span>
+
               </div>
 
               <div className="form-group">
                 <label htmlFor="password">Password</label>
                 <input type="password"
                        className="form-control" id="password" name="password"
-                       placeholder="Password"/>
+                       placeholder="Password"
+                       value={password}
+                       onChange={handlePasswordChange}
+                />
+                <span className="errorMessage">
+                  {submitted && passwordError() ? 'Password is required' : ''}
+
+                </span>
+
               </div>
 
 
@@ -48,6 +121,14 @@ const Styles = styled.div`
     .login{
         margin-bottom: 20rem;
         padding: 2rem;
+    }
+        .errorMessage {
+        color: red;
+        font-size: 0.85em;
+        font-weight: normal;
+    }
+    .error{
+    border: 1px solid #eb516d;
     }
 
 `;
--- src/components/views/Register.js
+++ src/components/views/Register.js
@@ -1,47 +1,174 @@
-import React from "react";
+import React, {useState} from 'react';
 import styled from "styled-components";
+import axios from "axios";
 
 
-const Register = () => {
 
-    return(
+
+const Register = () =>{
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
+  };
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
+  };
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
+
+  const firstNameError = () => {
+
+    return firstName === '' || !firstName.match(/(?=^.{3,}$)[a-zA-Z ]*$/);
+  };
+
+
+  const lastNameError = () => {
+    return lastName === '' || !lastName.match(/(?=^.{3,}$)[a-zA-Z ]*$/);
+  };
+  const usernameError = () => {
+    return username === '' ||!username.match(/(?=^.{3,}$)[a-zA-Z0-9 ]*$/);
+  };
+  const passwordError = () => {
+    return password === '' ||  !password.match(/(?=^.{6,}$)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/);
+  };
+  const confirmPasswordError = () => {
+    return confirmPassword === '' || confirmPassword !== password;
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+    if(firstNameError() || lastNameError() || usernameError() || passwordError() || confirmPasswordError()){
+      setError(true);
+      setSuccess(false);
+    }else {
+      // Password confirmation is only on the client side.
+      // There is no need to send the password confirmation field with the user object to the server.
+      const registerForm = {
+        firstName: firstName,
+        lastName: lastName,
+        username: username,
+        password: password,
+
+      };
+
+      setFirstName('');
+      setLastName('');
+      setUsername('');
+      setPassword('');
+      setConfirmPassword('');
+
+      console.log("NEW registerForm object: ", registerForm);
+      axios.post("", registerForm).then(res => {
+        console.log("#### register user response ", res);
+        setError(false);
+        setSuccess(true);
+        setSubmitted(false);
+
+      }).catch(err => {
+        console.log("Error: ", err);
+      });
+    }
+
+  };
+
+  return(
         <Styles>
           <div style={{marginTop:'50px'}} className="register">
             <h2 className="custom-form">Register</h2>
-            <form className="custom-form">
+            <form className="custom-form" onSubmit={onSubmit} >
               <div className="form-group">
                 <label htmlFor="firstName">First Name</label>
                 <input type="text"
-                       className="form-control" id="firstName" name="firstName"
-                       placeholder="First Name"/>
+                       className="form-control"
+                       id="firstName" name="firstName"
+                       placeholder="First Name"
+                       value={firstName}
+                       onChange={handleFirstNameChange}
+                />
+                <span className="errorMessage">
+                  {submitted && firstNameError() ? 'Enter at least 3 characters"' : ''}
+
+                </span>
               </div>
 
               <div className="form-group">
                 <label htmlFor="lastName">Last Name</label>
                 <input type="text"
-                       className="form-control" id="lastName" name="lastName"
-                       placeholder="Last Name"/>
+                       className={`form-control`}
+                       id="lastName" name="lastName"
+                       placeholder="Last Name"
+                       value={lastName}
+                       onChange={handleLastNameChange}
+                />
+                <span className="errorMessage">
+                    {submitted && lastNameError() ? 'Enter at least 3 characters' : ''}
+
+                </span>
+
               </div>
 
               <div className="form-group">
               <label htmlFor="username">Username</label>
               <input type="text"
-                     className="form-control" id="username" name="userName"
-                     placeholder="Username"/>
+                     className={`form-control`}
+                     id="username" name="userName"
+                     placeholder="Username"
+                     value={username}
+                     onChange={handleUsernameChange}
+
+              />
+                <span className="errorMessage">
+                    {submitted && usernameError() ? 'Enter at least 3 characters or numbers' : ''}
+                </span>
               </div>
 
               <div className="form-group">
                 <label htmlFor="password">Password</label>
                 <input type="password"
-                       className="form-control" id="password" name="password"
-                       placeholder="Password"/>
+                       className={`form-control`}
+                       id="password" name="password"
+                       placeholder="Password"
+                       value={password}
+                       onChange={handlePasswordChange}
+
+                />
+                <span className="errorMessage">
+                    {submitted && passwordError() ? 'Password must contain at least one number, one uppercase and 6 characters' : ''}
+
+                </span>
               </div>
 
               <div className="form-group">
                 <label htmlFor="confirmPassword">Confirm Password</label>
                 <input type="password"
-                       className="form-control" id="confirmPassword" name="confirmPassword"
-                       placeholder="Confirm Password"/>
+                       className={`form-control`}
+                       id="confirmPassword" name="confirmPassword"
+                       placeholder="Confirm Password"
+                       value={confirmPassword}
+                       onChange={handleConfirmPasswordChange}
+
+                />
+                <span className="errorMessage">
+                    {submitted && confirmPasswordError() ? 'Password do not match' : ''}
+                </span>
               </div>
 
               <div className="form-group">
@@ -59,6 +186,7 @@ const Register = () => {
     );
 
 
+
 };
 export default Register;
 
@@ -67,5 +195,15 @@ const Styles = styled.div`
         margin-bottom: 20rem;
         padding: 2rem;
     }
+    .errorMessage {
+        color: red;
+        font-size: 0.85em;
+        font-weight: normal;
+    }
+    .error{
+    border: 1px solid #eb516d;
+    }
 
 `;
+
+
